5cf5b3b5c5b5ee96462e3902ccf0b8e23f9fce0b
Yusufhan Keleş <yusudhan@gmail.com>
2023-08-01 13:38:06 +0300

    init commit
    some api problems

---
+++ LinguoMind/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:linguomind_assistant/screens/home_screen.dart';
+
+void main()
+{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'OpenAI ChatGPT Dale-E',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
